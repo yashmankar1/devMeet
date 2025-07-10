41694f639c18c0e1b670143c23a496a2b908dddf
yashmankar1 <yashmankar349@gmail.com>
2025-07-10 22:49:12 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/user", (req, res) => {
-  res.send("HHAHAHAHAHAH");
+  res.send("HHAHAHAHAHAH ");
 });
 
 app.get("/user", (req, res) => {
