292938b4d3040e2332f298b756c5f25872a6f508
yashmankar1 <deep.mankar@ranium.in>
2025-09-23 00:22:35 +0530

    Add CORS support and update auth login response

--- src/app.js
+++ src/app.js
@@ -7,7 +7,14 @@ const authRouter = require("./routers/auth");
 const profileRouter = require("./routers/profile");
 const requestRouter = require("./routers/request");
 const userRouter = require("./routers/user");
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -42,7 +42,7 @@ authRouter.post("/login", async (req, res) => {
         expires: new Date(Date.now() + 8 * 3600000),
       });
 
-      res.send("login Successfull!");
+      res.send(user);
     } else {
       throw new Error("Invalid credentials!");
     }
