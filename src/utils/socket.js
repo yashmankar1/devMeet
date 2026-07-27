d4f2f46ff7d81505fd44cbb421bb21d6ec4d36d7
Yash Mankar <yash@devmeet.dev>
2026-07-27 12:22:22 +0000

    fix: add CORS config to socket.io for Vercel and custom domain

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,45 +2,54 @@ const socket = require("socket.io");
 const crypto = require("crypto");
 const { Chat } = require("../models/chat");
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://devmeet-ten.vercel.app",
+  "https://devmeetup.me",
+  "https://www.devmeetup.me",
+];
+
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
     .createHash("sha256")
     .update([userId, targetUserId].sort().join("_"))
     .digest("hex");
 };
+
 const initializeSocket = (server) => {
-  const io = socket(server, { cors: { origin: "http://localhost:5173" } });
+  const io = socket(server, {
+    cors: {
+      origin: allowedOrigins,
+      credentials: true,
+      methods: ["GET", "POST"],
+    },
+  });
+
   io.on("connection", (socket) => {
     socket.on("joinChat", ({ userId, targetUserId }) => {
       const roomId = getSecretRoomId(userId, targetUserId);
-      console.log("Joining Room: " + roomId);
       socket.join(roomId);
     });
-    socket.on(
-      "sendMessage",
-      async ({ firstName, lastName, userId, targetUserId, text }) => {
-        try {
-          const roomId = getSecretRoomId(userId, targetUserId);
-          console.log(firstName + " " + text);
-          let chat = await Chat.findOne({
-            participants: { $all: [userId, targetUserId] },
-          });
-          if (!chat) {
-            chat = new Chat({
-              participants: [userId, targetUserId],
-              messages: [],
-            });
-          }
-          chat.messages.push({ senderId: userId, text });
 
-          await chat.save();
-          io.to(roomId).emit("messageReceived", { firstName, lastName, text });
-        } catch (error) {
-          console.log(error);
+    socket.on("sendMessage", async ({ firstName, lastName, userId, targetUserId, text }) => {
+      try {
+        const roomId = getSecretRoomId(userId, targetUserId);
+        let chat = await Chat.findOne({
+          participants: { $all: [userId, targetUserId] },
+        });
+        if (!chat) {
+          chat = new Chat({ participants: [userId, targetUserId], messages: [] });
         }
-      },
-    );
+        chat.messages.push({ senderId: userId, text });
+        await chat.save();
+        io.to(roomId).emit("messageReceived", { firstName, lastName, text });
+      } catch (error) {
+        console.error("Socket sendMessage error:", error);
+      }
+    });
+
     socket.on("disconnect", () => {});
   });
 };
+
 module.exports = initializeSocket;
